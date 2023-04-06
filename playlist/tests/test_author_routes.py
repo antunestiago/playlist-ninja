86b012088e177778d0e4da68e0ac3a3f274b3907
Tiago Antunes <tiagoantunes14@gmail.com>
2023-04-06 15:45:09 -0400

    Increase test coverage to author route

--- playlist/tests/test_author_routes.py
+++ playlist/tests/test_author_routes.py
@@ -5,8 +5,6 @@ import pytest
 from playlist.models import Author
 
 
-# CREATE AUTHOR TESTS
-
 @pytest.mark.django_db
 def test_create_author(client):
     url = reverse("api-1.0.0:create_author")
@@ -39,7 +37,6 @@ def test_list_authors(client):
     assert response_data[1]["name"] == "Author 2"
 
 
-# GET AUTHOR TESTS
 @pytest.mark.django_db
 def test_get_author(client):
     author = Author.objects.create(name="John Doe")
@@ -52,46 +49,57 @@ def test_get_author(client):
     assert response.status_code == 200
     assert response_data["name"] == "John Doe"
 
-#
-# def test_get_author_not_found(client):
-#     url = reverse("author-detail", kwargs={"author_id": 999})
-#
-#     response = client.get(url)
-#
-#     assert response.status_code == status.HTTP_404_NOT_FOUND
-#
-#
-# # UPDATE AUTHOR TESTS
-#
-# def test_update_author(client):
-#     author = Author.objects.create(name="John Doe", bio="A famous author", birthdate="1980-01-01")
-#     url = reverse("author-detail", kwargs={"author_id": author.id})
-#
-#     payload = {
-#         "name": "Jane Doe",
-#         "bio": "An even more famous author",
-#         "birthdate": "1970-01-01"
-#     }
-#
-#     response = client.put(url, json.dumps(payload), content_type="application/json")
-#
-#     assert response.status_code == status.HTTP_200_OK
-#     assert response.data["name"] == payload["name"]
-#     assert response.data["bio"] == payload["bio"]
-#     assert response.data["birthdate"] == payload["birthdate"]
-#
-#
-# def test_update_author_not_found(client):
-#     url = reverse("author-detail", kwargs={"author_id": 999})
-#
-#     payload = {
-#         "name": "Jane Doe",
-#         "bio": "An even more famous author",
-#         "birthdate": "1970-01-01"
-#     }
-#
-#     response = client.put(url, json.dumps(payload), content_type="application/json")
-#
-#     assert response.status_code == status.HTTP_404_NOT_FOUND
+
+@pytest.mark.django_db
+def test_get_author_not_found(client):
+    url = reverse("api-1.0.0:get_author", kwargs={"author_id": 999})
+
+    response = client.get(url)
+
+    assert response.status_code == 404
 
 
+@pytest.mark.django_db
+def test_update_author(client):
+    author = Author.objects.create(name="John Doe")
+    url = reverse("api-1.0.0:update_author", kwargs={"author_id": author.id})
+
+    payload = {
+        "name": "Caze",
+    }
+
+    response = client.put(url, json.dumps(payload), content_type="application/json")
+
+    response_data = response.json()
+
+    assert response.status_code == 200
+    assert response_data["name"] == payload["name"]
+
+@pytest.mark.django_db
+def test_update_author_not_found(client):
+    url = reverse("api-1.0.0:update_author", kwargs={"author_id": 999})
+
+    payload = {
+        "name": "Jane Doe",
+    }
+
+    response = client.put(url, json.dumps(payload), content_type="application/json")
+
+    assert response.status_code == 404
+
+@pytest.mark.django_db
+def test_delete_author(client):
+    author = Author.objects.create(name="John Doe")
+    url = reverse("api-1.0.0:delete_author", kwargs={"author_id": author.id})
+
+    response = client.delete(url, content_type="application/json")
+
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_delete_author(client):
+    url = reverse("api-1.0.0:delete_author", kwargs={"author_id": 999})
+
+    response = client.delete(url, content_type="application/json")
+
+    assert response.status_code == 404
